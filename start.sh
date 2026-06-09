4ac78819b726616dfa30d511bfa4d72fc07c279c
mohamed <ense1077710@tarbia.tn>
2026-06-09 23:30:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "Downloading Paper 26.1.2..."
-
-curl -L "https://fill-data.papermc.io/v1/objects/d30fae0c74092b10855f0412ca6b265c60301a013d34bc28a2a41bf5682dd80b/paper-26.1.2-69.jar" \
--o server.jar
+if [ ! -f server.jar ]; then
+  echo "Downloading Paper 26.1.2..."
+  curl -L \
+  "https://fill-data.papermc.io/v1/objects/d30fae0c74092b10855f0412ca6b265c60301a013d34bc28a2a41bf5682dd80b/paper-26.1.2-69.jar" \
+  -o server.jar
+fi
 
 echo "eula=true" > eula.txt
 
-java -Xms512M -Xmx1024M -jar server.jar nogui
+java -Xms128M -Xmx384M -jar server.jar nogui
