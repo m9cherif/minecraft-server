fe122dfb936acd6ec433c793d70ab040d58084ee
mohamed <ense1077710@tarbia.tn>
2026-06-09 23:14:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-echo "Checking Paper builds..."
+echo "Downloading Paper 26.1.2..."
 
-curl -s https://api.papermc.io/v2/projects/paper/versions/1.21.11
+curl -L \
+"https://fill-data.papermc.io/v1/objects/d30fae0c74092b10855f0412ca6b265c60301a013d34bc28a2a41bf5682dd80b/paper-26.1.2-69.jar" \
+-o server.jar
 
-sleep 300
+echo "Checking file..."
+ls -lh server.jar
+
+echo "eula=true" > eula.txt
+
+cat > server.properties <<EOF
+motd=Paper 26.1.2 Server
+max-players=3
+online-mode=true
+difficulty=normal
+view-distance=6
+simulation-distance=4
+EOF
+
+java -Xms512M -Xmx1024M -jar server.jar nogui
