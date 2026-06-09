2ee10d422a140835b1ddb568722dd89334c6c770
mohamed <ense1077710@tarbia.tn>
2026-06-09 23:04:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-curl -L https://api.papermc.io/v2/projects/paper -o test.txt
+PAPER_VERSION=1.21.11
 
-echo "===== CONTENT ====="
-cat test.txt
-echo "==================="
+echo "Downloading latest Paper for $PAPER_VERSION..."
 
-sleep 300
+curl -L \
+"https://fill.papermc.io/v1/objects/paper/${PAPER_VERSION}/latest/download" \
+-o server.jar
+
+echo "Downloaded file size:"
+ls -lh server.jar
+
+echo "eula=true" > eula.txt
+
+cat > server.properties << EOF
+motd=Render Minecraft Server
+max-players=3
+online-mode=true
+difficulty=normal
+spawn-protection=0
+view-distance=6
+simulation-distance=4
+EOF
+
+java -Xms512M -Xmx1024M -jar server.jar nogui
