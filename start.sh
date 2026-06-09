75a5762bf7a0ffc3e78532f1796b7b2f04602558
mohamed <ense1077710@tarbia.tn>
2026-06-09 22:59:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-PAPER_VERSION=26.1.2
-PAPER_BUILD=151
+curl -L https://api.papermc.io/v2/projects/paper -o test.txt
 
-if [ ! -f server.jar ]; then
-  echo "Downloading Paper..."
-  curl -L -o server.jar https://api.papermc.io/v2/projects/paper/versions/${PAPER_VERSION}/builds/${PAPER_BUILD}/downloads/paper-${PAPER_VERSION}-${PAPER_BUILD}.jar
-  ls -lh server.jar
-  cat server.jar
-fi
+echo "===== CONTENT ====="
+cat test.txt
+echo "==================="
 
-if [ ! -f eula.txt ]; then
-  echo "eula=true" > eula.txt
-fi
-
-if [ ! -f server.properties ]; then
-cat <<EOF > server.properties
-motd=Render Minecraft Server
-max-players=3
-online-mode=true
-difficulty=normal
-spawn-protection=0
-view-distance=6
-simulation-distance=4
-EOF
-fi
-
-java -Xms512M -Xmx1024M -jar server.jar nogui
+sleep 300
