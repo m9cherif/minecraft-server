65ca6ad776ca8e738647c202ae609087ac908d55
mohamed <ense1077710@tarbia.tn>
2026-06-09 23:32:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# خادم HTTP بسيط لـ Render
+python3 -m http.server ${PORT:-10000} &
+
 if [ ! -f server.jar ]; then
-  echo "Downloading Paper 26.1.2..."
-  curl -L \
-  "https://fill-data.papermc.io/v1/objects/d30fae0c74092b10855f0412ca6b265c60301a013d34bc28a2a41bf5682dd80b/paper-26.1.2-69.jar" \
+  curl -L "https://fill-data.papermc.io/v1/objects/d30fae0c74092b10855f0412ca6b265c60301a013d34bc28a2a41bf5682dd80b/paper-26.1.2-69.jar" \
   -o server.jar
 fi
 
